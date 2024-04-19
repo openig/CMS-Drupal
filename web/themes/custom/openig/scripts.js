094b7d0d538dcf9afd715dabf2a40e922aacd032
Lucie Lorel <llorel@tokyo.teicee.fr>
2024-04-19 15:42:01 +0200

    style: affichage du header #36

---
+++ web/themes/custom/openig/scripts.js
@@ -0,0 +1,7 @@
+(function($){
+    $(document).ready(function(){
+        $('#search_icon').click(function() {
+            $('#search-block-form').toggleClass('open');
+        });
+    })
+})(jQuery);
