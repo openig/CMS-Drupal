995cc7aa66b154aa6cbfe7904b039c0e0ec16122
Lucie Lorel <llorel@tokyo.teicee.fr>
2024-04-22 15:17:34 +0200

    style: affichage du header, sous-menu et liens actifs #36

--- web/themes/custom/openig/scripts.js
+++ web/themes/custom/openig/scripts.js
@@ -3,5 +3,13 @@
         $('#search_icon').click(function() {
             $('#search-block-form').toggleClass('open');
         });
+
+        if($('.dropdown-item').hasClass('is-active')){
+            var parent = $('.dropdown-item.is-active').closest('.nav-item.dropdown');
+            var link = $(parent).find('.dropdown-toggle');
+            $(link).addClass('show');
+            var child = $(parent).find('.dropdown-menu');
+            $(child).addClass('show');
+        }
     })
 })(jQuery);
