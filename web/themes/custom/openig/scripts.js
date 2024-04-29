bc514e0e51e60d9c70395105e8cbb032c70b2079
Lucie Lorel <llorel@tokyo.teicee.fr>
2024-04-29 12:56:57 +0200

    style: ajout de commentaires #44

--- web/themes/custom/openig/scripts.js
+++ web/themes/custom/openig/scripts.js
@@ -1,10 +1,12 @@
 (function($){
     $(document).ready(function(){
+        // Afficher/masquer barre de recherche
         $('#search_icon').click(function() {
             $('#search-block-form').toggleClass('open');
             $('#search_icon').toggleClass('open');
         });
 
+        // Garder le sous-menu affiché si l'un de ses liens est actifs
         if($('.dropdown-item').hasClass('is-active')){
             var parent = $('.dropdown-item.is-active').closest('.nav-item.dropdown');
             var link = $(parent).find('.dropdown-toggle');
@@ -12,6 +14,8 @@
             var child = $(parent).find('.dropdown-menu');
             $(child).addClass('show');
         }
+
+        // Afficher/masquer icônes bouton partager
         $('#block-openig-addtoanysharebuttons #button_share').click(function () {
             $('#block-openig-addtoanysharebuttons .addtoany_list').toggleClass('open');
         })
