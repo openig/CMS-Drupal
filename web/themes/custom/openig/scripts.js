3e9ce8fd42280d84d3d2d33fce8850027ee7f5fe
Noémie Lejuez <nlejuez@teicee.com>
2024-07-05 16:46:38 +0200

    Correctif: Mise à jour du badge suivant les fitres - traitement pour le type de contenu
    Refs: #142

--- web/themes/custom/openig/scripts.js
+++ web/themes/custom/openig/scripts.js
@@ -159,9 +159,22 @@
 
         // Badge nombre de résultat - onglet contenu éditorial
         let onglet = $('#block-openig-local-tasks #nbContenuEditorial');
-        let nbrElement = onglet.data('id');
+        let nbrElementNoFilter = onglet.data('id');
+
         let badge = $('#block-openig-local-tasks #nbContenuEditorial a');
+
+        let headerSearch = $('.view-resultats-de-recherche #nbrContenu');
+        let nbrElementFilter = headerSearch.data('id');
+
+        var nbrElement = "";
+        if(nbrElementFilter == undefined)
+          nbrElement = nbrElementNoFilter;
+        else
+          nbrElement = nbrElementFilter;
+
         badge.append("<span class='badge rounded-pill bg-warning text-black'>"+nbrElement+"</span>");
+
+
 
 
         // Affichage du panneau de filtre
