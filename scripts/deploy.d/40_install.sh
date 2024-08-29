dfe3add418a205e305ee115fce1c5f5571a4641a
Website openig_web <git@openig.org>
2024-08-29 12:01:58 +0000

    Update 10.3.1 vers 10.3.2

--- scripts/deploy.d/40_install.sh
+++ scripts/deploy.d/40_install.sh
@@ -30,7 +30,7 @@ init_d10_status() {
 	[ -z "${BIN_DRUSH}" ] && p_warning "Commande Drush non disponible (aucune configuration Drupal effectuée) !" && return 0
 	
 	## attente de disponibilité de la base de données
-	! is_database_ready && p_warning "Base de données indisponible !" && return 1
+#	! is_database_ready && p_warning "Base de données indisponible !" && return 1
 	
 	p_section "Nettoyage complet des caches de l'instance Drupal..."
 	${BIN_DRUSH} cache:rebuild 2>/dev/null || true
